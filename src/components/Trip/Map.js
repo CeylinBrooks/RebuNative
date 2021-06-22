582f7058d37b72d1646c283e970ed6875bcced96
Brendan Smith <brendan5320@gmail.com>
2021-06-22 14:19:06 -0500

    built out Map component, takes lat/long for destination and origin as props

--- src/components/Trip/Map.js
+++ src/components/Trip/Map.js
@@ -1,93 +1,108 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import * as Location from 'expo-location';
 import MapView, { Marker } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 
-export default function Map() {
 
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [region, setRegion] = useState(null);
-  const [marker, setMarker] = useState(null);
-  const [mapView, setMapView] = useState(null);
+// Directions vars
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBtLbow5RiE2qmYmc1iqRcQnKnqfLZalKo'; // this needs to go in .env
 
-  const origin = { latitude: 37.3318456, longitude: -122.0296002 };
-  const destination = { latitude: 37.771707, longitude: -122.4053769 };
-  const GOOGLE_MAPS_APIKEY = 'AIzaSyBtLbow5RiE2qmYmc1iqRcQnKnqfLZalKo';
+export default class Map extends Component {
+  constructor(props) {
+    super(props);
 
+    // State
+    this.state = {
+      location: null,
+      errorMsg: null,
+      region: null,
+      origin: props.origin,
+      destination: props.destination,
+      distance: null,
+      duration: null,
+    };
 
-  useEffect(() => {
+    this.mapView = null;
+  }
+
+  // Get current user location upon load
+  componentDidUpdate() {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setRegion({ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0421, longitudeDelta: 0.0421 });
-      setMarker({ latitude: location.coords.latitude, longitude: location.coords.longitude });
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+      this.setState({ location: location });
+    });
   }
 
-  return (
-      <MapView
-        style={styles.map}
-        region={region}
-        ref={c => { setMapView(c) }}
-      >
-        <Marker coordinate={origin} />
-        <Marker coordinate={destination} />
-        <MapViewDirections
-          origin={origin}
-          destination={destination}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={3}
-          strokeColor="#00a88a"
-        // onReady={result => {
-        //   console.log(mapView);
-        //   console.log(`Distance: ${result.distance} km`);
-        //   console.log(`Duration: ${result.duration} min.`);
-        //   mapView.fitToCoordinates(result.coordinates, {
-        //     edgePadding: {
-        //       right: 20,
-        //       bottom: 20,
-        //       left: 20,
-        //       top: 20
-        //     }
-        //   });
-        // }}
-        />
-      </MapView>
-      // <Text>{text}</Text>
-      // <StatusBar style="auto" />
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // };
 
-  );
+  render() {
+    return (
+      <View style={styles.view} >
+        <MapView
+          style={styles.map}
+          region={this.state.region}
+          showsUserLocation={true}
+          ref={ref => { this.mapView = ref }}
+          loadingEnabled={true}
+          loadingBackgroundColor={'#00a88a'}
+        >
+          <Marker coordinate={this.state.origin} />
+          <Marker coordinate={this.state.destination} />
+          <MapViewDirections
+            origin={this.state.origin}
+            destination={this.state.destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={5}
+            strokeColor="#00a88a"
+            onReady={result => {
+              console.log(`Distance: ${result.distance} km`);
+              console.log(`Duration: ${result.duration} min.`);
+              this.setState({ distance: result.distance, duration: result.duration });
+              this.mapView.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: 20,
+                  bottom: 20,
+                  left: 20,
+                  top: 20
+                }
+              });
+            }}
+          />
+        </MapView>
+        <Text style={styles.text}>Distance: {Math.round(this.state.distance)} km</Text>
+        <Text style={styles.text}>Duration: {Math.round(this.state.duration)} min</Text>
+        {/* pricing is arbitray for demo */}
+        <Text style={styles.text}>Estimated Cost: ${Math.round(this.state.duration * this.state.distance / 30)}</Text> 
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00a88a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    fontFamily: "Helvetica",
-    fontSize: 40,
-    color: "#fff",
+  view: {
+    width: "100%",
+    height: "70%",
+    alignItems: "center"
   },
   map: {
     width: "90%",
-    height: "50%",
+    height: "80%",
+    borderRadius: 6
   },
+  text: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold"
+  }
 });
